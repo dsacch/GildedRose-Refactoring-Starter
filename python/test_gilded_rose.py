3f51de93b862bb5138358d3e5be74697ced3f872
Domenic Sacchetti <67714641+dsacch@users.noreply.github.com>
2022-09-14 11:10:04 -0500

    Refactored a bit and added tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,24 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_Aged_Brie(self):
+        items = [Item(name="Aged Brie", sell_in=0, quality=0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("Aged Brie", items[0].name)
+        self.assertEquals(2, items[0].quality)
+    
+    def test_Sulfuras(self):
+        items = [Item("Sulfuras", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("Sulfuras", items[0].name)
+
+    def test_Backstage_Passes(self):
+        items = [Item("Backstage passes", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("Backstage passes", items[0].name)
 
         
 if __name__ == '__main__':
